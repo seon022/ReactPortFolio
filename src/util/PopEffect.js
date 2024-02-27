8fb4636a0d135209289a799ecf5081bf3adb0ee7
seon022 <seon022@icloud.com>
2024-02-27 17:36:21 +0900

    0227 popAni Start

--- src/util/PopEffect.js
+++ src/util/PopEffect.js
@@ -4,10 +4,10 @@ import styled from "styled-components";
 export const PopEffect = ({children})=>{
     const Anim = styled.div`
         @keyframes PopAni {
-        0% {transform: translate(0);}
-        25% {transform: translate(8px);}
-        50% {transform: translate(0);}
-        75% {transform: translate(8px);}
+            0% {transform: translate(0);}
+            25% {transform: translate(8px);}
+            50% {transform: translate(0);}
+            75% {transform: translate(8px);}
         }
     `
     return(
